7fe6ca0cb3eb89061100a75337758aa6aa7bd4dd
dichroic7 <104331454+dichroic7@users.noreply.github.com>
2022-04-25 13:56:33 +0900

    Create Hello.java

---
+++ java/src/day1/Hello.java
@@ -0,0 +1,12 @@
+package day1;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello world!!");
+		System.out.println("Hello world!!");
+		System.out.print("Hello world!!");
+	}
+
+}
